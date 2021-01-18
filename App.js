a363e4bda32a39533e6bb7e1f790fb3d1c3e7fdf
karlkukk <karlkukk97@gmail.com>
2021-01-18 21:53:52 +0200

    Õpetaja vahetamine

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 
-const tunniplaan = new Tunniplaan(346, '2019-04-01');
+const tunniplaan = new Tunniplaan('346', '2019-04-01');
 const kl = new Kasutajaliides();
 
 document.addEventListener('DOMContentLoaded', valjastatudTunniplaan);
+
+const changeBtn = document.querySelector('#u-opetaja');
+changeBtn.addEventListener('click', uusOpetaja);
 
 function valjastatudTunniplaan(){
     tunniplaan.opetajaTunniplaaniAndmed().then(tunnid => {
         kl.printNadalakp(kl.nadalaKuupaevad(tunnid));
         kl.print(tunnid);
+
     }).catch(viga => console.log(viga));
 }
+
+function uusOpetaja() {
+    const opetajaId = document.querySelector('#opetajaId').value;
+    tunniplaan.opetaja = opetajaId;
+    valjastatudTunniplaan();
+
+}
+
+
+
